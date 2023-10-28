513ca22d6ae42ad911c316cfb5e7eda6f875e8d3
tamlt2704 <tamlt2704@gmail.com>
2023-10-28 10:21:33 +0800

    using profile

--- address/src/main/java/com/app/controller/AddressController.java
+++ address/src/main/java/com/app/controller/AddressController.java
@@ -4,6 +4,7 @@ import com.app.dto.AddressResponse;
 import com.app.dto.CreateAddressRequest;
 import com.app.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,4 +29,12 @@ public class AddressController {
         return addressService.getById(id);
     }
 
+
+    @Value("${address.env}")
+    private String env;
+
+    @GetMapping("/test")
+    public String test() {
+        return env;
+    }
 }
